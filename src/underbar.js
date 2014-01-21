270a58ad8069bdcff59c97616f01b289b613329f
Adrian <uknoiluv@gmail.com>
2014-01-21 23:59:23 +0900

    Second commit

--- src/underbar.js
+++ src/underbar.js
@@ -29,7 +29,10 @@ var _ = { };
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array[array.length - 1] : array.slice(0,n);
+    if (n === 0)
+      return [];
+    else
+      return n === undefined ? array[array.length - 1] : array.slice(- n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -38,6 +41,14 @@ var _ = { };
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(var i = 0; i < collection.length; i ++)
+        iterator(collection[i],i,collection);
+    }
+    else{
+      for(var prop in collection)
+        iterator(collection[prop],prop,collection);
+    }          
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -59,10 +70,32 @@ var _ = { };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [ ];
+
+    _.each(collection, function(item){
+      if(test(item)){
+        result.push(item);
+      }      
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
-  _.reject = function(collection, test) {
+  _.reject = function(collection, test) {  
+    var result = [ ];
+
+      function negate(func) {
+        return function(x) {
+          return !func(x);
+        };
+      }
+
+    var negateTest = negate(test);
+
+      result = _.filter(collection, negateTest);
+
+    return result;
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
   };
